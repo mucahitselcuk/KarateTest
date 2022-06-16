260752745f4aed4b0458ff40cda896ecb2093736
Mücahit Selçuk <mucahitselcuk25@gmail.com>
2022-06-16 16:37:19 +0300

    Commit

--- src/test/java/KarateUI/Test/KarateRunner.java
+++ src/test/java/KarateUI/Test/KarateRunner.java
@@ -1,10 +1,9 @@
 package KarateUI.Test;
 
-import com.intuit.karate.driver.WebDriver;
 import com.intuit.karate.junit5.Karate;
 
 class KarateRunner {
-    public static WebDriver driver;
+
 
     @Karate.Test
     Karate testUsers() {
